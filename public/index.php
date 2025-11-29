1e224565a99ac3a686b472fd3da37ab0481c698f
Yasser Taha <yasser1119992017@gmail.com>
2025-11-29 19:56:08 +0200

    implament autoload files

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+// use App\Oop\Test2;
+
+// $info = new Test2('Yasser Taha', 27, 'Jaza Egypt');
+// echo $info->printInfo();
---
+++ src/oop/Test2.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Oop;
+    
+class Test2
+{
+    private string $name;
+    private int  $age;
+    private string $adress;
+
+    public function __construct($name,  $age,  $adress){
+        // throw new \Exception('Not implemented');
+        $this->name = $name;
+        $this->age = $age;
+        $this->adress = $adress;
+        // $this->printInfo();
+    }
+    public function printInfo(){
+        return "My Name is {$this->name} , I have {$this->age} Years Old, live in {$this->adress}";
+    }
+}
